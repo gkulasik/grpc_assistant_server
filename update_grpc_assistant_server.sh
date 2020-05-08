48b95a2393ef86ba25b7e558de6691da3527236d
Greg Kulasik <gkulasik@gmail.com>
2020-05-08 08:30:47 -0500

    Set targets for docker to latest containers

--- update_grpc_assistant_server.sh
+++ update_grpc_assistant_server.sh
@@ -2,5 +2,5 @@
 sh ./stop_grpc_assistant_server.sh
 echo "GRPC assistant server updating..."
 git pull
-docker pull gkulasik/grpc_assistant_server:0.2.0
+docker pull gkulasik/grpc_assistant_server:latest
 echo "GRPC assistant server updated."
